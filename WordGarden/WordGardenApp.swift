f14182a836addfa656a407a0c7a877741129f701
varun-singh26 <149445092+varun-singh26@users.noreply.github.com>
2024-09-21 22:38:37 -0400

    Initial Commit

---
+++ WordGarden/WordGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGardenApp.swift
+//  WordGarden
+//
+//  Created by Varun Singh on 9/21/24.
+//
+
+import SwiftUI
+
+@main
+struct WordGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
