a12a511813de493ea84dec13dfee1925f120f3ab
varun-singh26 <149445092+varun-singh26@users.noreply.github.com>
2024-09-23 02:22:45 -0400

    1st Commit

--- WordGarden/ContentView.swift
+++ WordGarden/ContentView.swift
@@ -8,14 +8,89 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var wordsGuessed = 0
+    @State private var wordsMissed = 0
+    @State private var wordsToGuess = ["SWIFT", "DOG", "CAT"]
+    @State private var currentWord = 0
+    @State private var gameStatusMessage = "How many Guesses to Uncover the Hidden Word"
+    @State private var guessedLetter = ""
+    @State private var imageName = "flower8"
+    @State private var playAgainHidden = true
+    @FocusState private var textFieldIsFocused: Bool
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack {
+                VStack (alignment: .leading) {
+                    Text("Words Guessed: \(wordsGuessed)")
+                    Text("Words Missed: \(wordsMissed)")
+                }
+                Spacer()
+                VStack (alignment: .trailing) {
+                    Text("Words to Guess: \(wordsToGuess.count - (wordsGuessed + wordsMissed))")
+                    Text("Words in Game: \(wordsToGuess.count)")
+                }
+            }
+            .padding(.horizontal)
+            Spacer()
+            
+            Text(gameStatusMessage)
+                .font(.title)
+                .multilineTextAlignment(.center)
+                .padding()
+            
+            Spacer()
+            //TODO: Switch to wordsToGuess[currentWord]
+            Text("_ _ _ _")
+                .font(.title)
+            
+            if playAgainHidden {
+                HStack{
+                    TextField("", text: $guessedLetter)
+                        .textFieldStyle(.roundedBorder)
+                        .frame(width: 30)
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 5)
+                                .stroke(.gray, lineWidth: 2)
+                        }
+                        .keyboardType(.asciiCapable)
+                        .submitLabel(.done)
+                        .autocorrectionDisabled()
+                        .textInputAutocapitalization(.characters)
+                        .onChange(of: guessedLetter) {
+                            guessedLetter = guessedLetter.trimmingCharacters(in: .letters.inverted)
+                            guard let lastChar = guessedLetter.last
+                                else {
+                                return
+                            }
+                            guessedLetter = String(lastChar).uppercased()
+                        }
+                        .focused($textFieldIsFocused)
+                    
+                    Button("Guess a Letter") {
+                        //TODO: Guess a Letter Button action here
+                        textFieldIsFocused = false
+                    }
+                    .buttonStyle(.bordered)
+                    .tint(.mint)
+                    .disabled(guessedLetter.isEmpty)
+                }
+            }
+            
+            else {
+                Button("Another Word?") {
+                    //TODO: Another Word Button action here
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.mint)
+            }
+            
+            Spacer()
+            
+            Image(imageName)
+                .resizable()
+                .scaledToFit()
         }
-        .padding()
+        .ignoresSafeArea(edges: .bottom)
     }
 }
 
